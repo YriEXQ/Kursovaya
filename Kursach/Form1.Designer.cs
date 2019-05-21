b3a455d128081417a20a3ae1bf3375cd8ac0c2fb
EXZ <EXZ@DESKTOP-PQAGO3V>
2019-05-21 21:28:43 +0400

    Сделано все, ошибок не замечено

---
+++ Kursach/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Kursach
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.l_Files = new System.Windows.Forms.Label();
+            this.l_Limit = new System.Windows.Forms.Label();
+            this.l_Found = new System.Windows.Forms.Label();
+            this.lb_Files = new System.Windows.Forms.ListBox();
+            this.tb_Limit = new System.Windows.Forms.TextBox();
+            this.b_Start = new System.Windows.Forms.Button();
+            this.l_FoundCount = new System.Windows.Forms.Label();
+            this.b_AddFiles = new System.Windows.Forms.Button();
+            this.cb_ChunkSize = new System.Windows.Forms.ComboBox();
+            this.l_ChunkSize = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // l_Files
+            // 
+            this.l_Files.AutoSize = true;
+            this.l_Files.Location = new System.Drawing.Point(12, 9);
+            this.l_Files.Name = "l_Files";
+            this.l_Files.Size = new System.Drawing.Size(84, 13);
+            this.l_Files.TabIndex = 0;
+            this.l_Files.Text = "Выбор файлов:";
+            // 
+            // l_Limit
+            // 
+            this.l_Limit.AutoSize = true;
+            this.l_Limit.Location = new System.Drawing.Point(151, 9);
+            this.l_Limit.Name = "l_Limit";
+            this.l_Limit.Size = new System.Drawing.Size(112, 13);
+            this.l_Limit.TabIndex = 1;
+            this.l_Limit.Text = "Граница совпадения";
+            // 
+            // l_Found
+            // 
+            this.l_Found.AutoSize = true;
+            this.l_Found.Location = new System.Drawing.Point(151, 67);
+            this.l_Found.Name = "l_Found";
+            this.l_Found.Size = new System.Drawing.Size(95, 13);
+            this.l_Found.TabIndex = 1;
+            this.l_Found.Text = "Найдено файлов:";
+            // 
+            // lb_Files
+            // 
+            this.lb_Files.FormattingEnabled = true;
+            this.lb_Files.Location = new System.Drawing.Point(15, 26);
+            this.lb_Files.Name = "lb_Files";
+            this.lb_Files.Size = new System.Drawing.Size(120, 186);
+            this.lb_Files.TabIndex = 2;
+            // 
+            // tb_Limit
+            // 
+            this.tb_Limit.Location = new System.Drawing.Point(154, 26);
+            this.tb_Limit.Name = "tb_Limit";
+            this.tb_Limit.Size = new System.Drawing.Size(109, 20);
+            this.tb_Limit.TabIndex = 3;
+            this.tb_Limit.Text = "0";
+            // 
+            // b_Start
+            // 
+            this.b_Start.Location = new System.Drawing.Point(154, 218);
+            this.b_Start.Name = "b_Start";
+            this.b_Start.Size = new System.Drawing.Size(109, 23);
+            this.b_Start.TabIndex = 4;
+            this.b_Start.Text = "Запуск";
+            this.b_Start.UseVisualStyleBackColor = true;
+            this.b_Start.Click += new System.EventHandler(this.b_Start_Click);
+            // 
+            // l_FoundCount
+            // 
+            this.l_FoundCount.AutoSize = true;
+            this.l_FoundCount.Location = new System.Drawing.Point(151, 80);
+            this.l_FoundCount.Name = "l_FoundCount";
+            this.l_FoundCount.Size = new System.Drawing.Size(13, 13);
+            this.l_FoundCount.TabIndex = 1;
+            this.l_FoundCount.Text = "0";
+            // 
+            // b_AddFiles
+            // 
+            this.b_AddFiles.Location = new System.Drawing.Point(15, 218);
+            this.b_AddFiles.Name = "b_AddFiles";
+            this.b_AddFiles.Size = new System.Drawing.Size(120, 23);
+            this.b_AddFiles.TabIndex = 4;
+            this.b_AddFiles.Text = "Добавить файлы";
+            this.b_AddFiles.UseVisualStyleBackColor = true;
+            this.b_AddFiles.Click += new System.EventHandler(this.b_AddFiles_Click);
+            // 
+            // cb_ChunkSize
+            // 
+            this.cb_ChunkSize.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cb_ChunkSize.FormattingEnabled = true;
+            this.cb_ChunkSize.Items.AddRange(new object[] {
+            "2",
+            "4",
+            "6",
+            "8"});
+            this.cb_ChunkSize.Location = new System.Drawing.Point(154, 119);
+            this.cb_ChunkSize.Name = "cb_ChunkSize";
+            this.cb_ChunkSize.Size = new System.Drawing.Size(109, 21);
+            this.cb_ChunkSize.TabIndex = 6;
+            this.cb_ChunkSize.SelectedIndexChanged += new System.EventHandler(this.cb_ChunkSize_SelectedIndexChanged);
+            // 
+            // l_ChunkSize
+            // 
+            this.l_ChunkSize.AutoSize = true;
+            this.l_ChunkSize.Location = new System.Drawing.Point(151, 103);
+            this.l_ChunkSize.Name = "l_ChunkSize";
+            this.l_ChunkSize.Size = new System.Drawing.Size(89, 13);
+            this.l_ChunkSize.TabIndex = 1;
+            this.l_ChunkSize.Text = "Размер шингла:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.cb_ChunkSize);
+            this.Controls.Add(this.b_AddFiles);
+            this.Controls.Add(this.b_Start);
+            this.Controls.Add(this.tb_Limit);
+            this.Controls.Add(this.lb_Files);
+            this.Controls.Add(this.l_FoundCount);
+            this.Controls.Add(this.l_ChunkSize);
+            this.Controls.Add(this.l_Found);
+            this.Controls.Add(this.l_Limit);
+            this.Controls.Add(this.l_Files);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label l_Files;
+        private System.Windows.Forms.Label l_Limit;
+        private System.Windows.Forms.Label l_Found;
+        private System.Windows.Forms.ListBox lb_Files;
+        private System.Windows.Forms.TextBox tb_Limit;
+        private System.Windows.Forms.Button b_Start;
+        private System.Windows.Forms.Label l_FoundCount;
+        private System.Windows.Forms.Button b_AddFiles;
+        private System.Windows.Forms.ComboBox cb_ChunkSize;
+        private System.Windows.Forms.Label l_ChunkSize;
+    }
+}
+
---
+++ Kursach/Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Kursach
+{
+    public partial class Form1 : Form
+    {
+        //=======Данные=======//
+        float MaxPercent = 0;
+        private static bool[] _Table;
+        private int CHUNCKSIZE = 2;
+        string Original = "";
+        string Compare = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            InititializeTable();
+            cb_ChunkSize.SelectedIndex = 0;
+        }
+
+
+        //=======Служебные методы=======//
+        private void InititializeTable()
+        {
+            _Table = new bool[65536];
+            for (char c = '0'; c <= '9'; c++) _Table[c] = true;
+            for (char c = 'A'; c <= 'Z'; c++) _Table[c] = true;
+            for (char c = 'a'; c <= 'z'; c++) _Table[c] = true;
+            _Table['.'] = true; _Table['('] = true; _Table[')'] = true;
+            _Table['>'] = true; _Table['<'] = true; _Table['='] = true;
+            _Table['-'] = true; _Table['+'] = true; _Table['*'] = true;
+            _Table['/'] = true; _Table['!'] = true; _Table['%'] = true;
+            _Table['&'] = true; _Table['|'] = true; _Table['~'] = true;
+            _Table[' '] = true; _Table['\\'] = true;
+        }
+
+        private static string Сanonize(string str)
+        {
+            char[] buffer = new char[str.Length];
+            int index = 0;
+            foreach (char c in str)
+            {
+                if (_Table[c])
+                {
+                    buffer[index] = c;
+                    index++;
+                }
+
+            }
+            return new string(buffer, 0, index);
+        }
+
+        private void SplitIntoShingles(ref String[] arShingles, ref String[] arWords, int chunkSize)
+        {
+            for (int j = 0; j < arWords.Length - chunkSize + 1; j++)
+            {
+
+                for (int i = 0; i < chunkSize && (j < arWords.Length - chunkSize + 1); i++)
+                    arShingles[j] += arWords[i + j];
+            }
+        }
+
+        private float DoShinglesAlghoritm()
+        {
+
+            if (this.Original.Length == 0 || this.Compare.Length == 0)
+            {
+                return -1;
+            }
+
+
+            //ПОДГОТОВКА ИСХОДНОГО ТЕКСТА(Надо проверить на то что текст вообще есть)
+            this.Original = Сanonize(this.Original); //Канонизация
+            this.Original = string.Join("`", Original.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
+            String[] arWordsOriginal = new String[Original.Length];
+            arWordsOriginal = this.Original.Split('`'); //Разбиение текста на слова
+            if (arWordsOriginal.Length < CHUNCKSIZE)
+            {
+                return -1;
+            }
+            String[] arShinglesOriginal = new String[arWordsOriginal.Length - CHUNCKSIZE + 1];
+            SplitIntoShingles(ref arShinglesOriginal, ref arWordsOriginal, CHUNCKSIZE); //Разбиение текста на шинглы
+
+
+            //ПОДГОТОВКА СРАВНИВАЕМОГО ТЕКСТА(Надо проверить на то что текст вообще есть)
+            this.Compare = Сanonize(this.Compare); //Канонизация
+            this.Compare = string.Join("`", Compare.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
+            String[] arWordsCompare = new String[Compare.Length];
+            arWordsCompare = this.Compare.Split('`'); //Разбиение текста на слова
+            if (arWordsCompare.Length < CHUNCKSIZE)
+            {
+                return -1;
+            }
+            String[] arShinglesCompare = new String[arWordsCompare.Length - CHUNCKSIZE + 1];
+            SplitIntoShingles(ref arShinglesCompare, ref arWordsCompare, CHUNCKSIZE); //Разбиение текста на шинглы
+
+
+            //Считаем количество совпадений
+            float MinLenght = arShinglesOriginal.Length;
+            float CountOfСoincidence = 0;
+            if (MinLenght > arShinglesCompare.Length) MinLenght = arShinglesCompare.Length;
+            for (int i = 0; i < MinLenght; i++)
+            {
+                if (arShinglesOriginal[i].GetHashCode() == arShinglesCompare[i].GetHashCode()) CountOfСoincidence++;
+            }
+            //Процент совпадений
+            return (CountOfСoincidence / MinLenght) * 100;
+            
+        }
+
+        private void b_Start_Click(object sender, EventArgs e)
+        {
+            //=======Блока с проверками=======//
+
+            //Проверка на выделенность
+            if (lb_Files.Items.Count == 0 || lb_Files.SelectedItem == null)
+            {
+                MessageBox.Show("Не выбран файл или их нет.", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            //Проверка на правильность MaxPercent если 
+            try
+            { MaxPercent = float.Parse(tb_Limit.Text); }
+            catch
+            {
+                MessageBox.Show("Введите число в границу совпадения.", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            //=======Выполнение алгоритма=======//
+            //Читаем в Original текст из файла
+            using (StreamReader sr = new StreamReader(lb_Files.SelectedItem.ToString()))
+            {
+                this.Original = sr.ReadToEnd();
+            }
+
+            //Цикл по всем файлам из ListBox
+            for (int i=0;i<lb_Files.Items.Count;i++)
+            {
+                if (i == lb_Files.SelectedIndex) continue; //Проверка если попали на Original
+                //Читаем в Compare текст из файла
+                using (StreamReader sr = new StreamReader(lb_Files.Items[i].ToString()))
+                {
+                    this.Compare = sr.ReadToEnd();
+                }
+                //Выполняем алгоритм
+                double PercentResult = DoShinglesAlghoritm();
+                //Если все правильно, то записываем в файл result результат проверки
+                if(PercentResult != -1 && PercentResult>MaxPercent)
+                { 
+                    using (StreamWriter sw = new StreamWriter("result.txt", false, System.Text.Encoding.Default))
+                    {
+                        sw.WriteLine(i + "." + lb_Files.Items[i].ToString() +"\t"+ PercentResult + "%");
+                    }
+                }
+                //Иначе вывподим ошибку
+                else MessageBox.Show($"Недостаточно текста в файле {lb_Files.Items[i].ToString()}", "Ошибка",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+
+            //Вывод MessageBox с успехом
+            MessageBox.Show("Операции успешно выполнены и сохранены в файл result.txt в каталоге с программой.", "Успех",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+        }
+
+        private void b_AddFiles_Click(object sender, EventArgs e)
+        {
+            //=======Запускаем диалог для выбора папки=======//
+            OpenFileDialog ofd = new OpenFileDialog();
+            string path = "";
+            using (var dialog = new FolderBrowserDialog())
+                if (dialog.ShowDialog() == DialogResult.OK)
+                    //Присваиваем путь выбранной папки
+                    path = dialog.SelectedPath;
+
+            //=======Добавляем файлы в ListBox=======//
+            if (path == "") return;
+            //Создаем массив с путями к файлам .txt формата
+            string[] Files = Directory.GetFiles(path, "*.txt");
+            if (Files.Length == 0)
+            {
+                MessageBox.Show("В папке нет файлов.", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            //Добавляем это в ListBox
+            lb_Files.Items.AddRange(Files);
+            //Выбираем первый файл автоматически выделенным в ListBox
+            lb_Files.SelectedIndex = 0;
+            //Выводим количество найденых файлов
+            l_FoundCount.Text = Files.Length.ToString();
+        }
+
+        private void cb_ChunkSize_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (cb_ChunkSize.Text)
+            {
+                case "2":
+                    CHUNCKSIZE = 2;
+                    break;
+                case "4":
+                    CHUNCKSIZE = 4;
+                    break;
+                case "6":
+                    CHUNCKSIZE = 6;
+                    break;
+                case "8":
+                    CHUNCKSIZE = 8;
+                    break;
+                default:
+                    CHUNCKSIZE = 2;
+                    break;
+            }
+        }
+    }
+}
